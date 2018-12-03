1bbf2d2a82f13ed9a35d49d2b616dcc07760251c
Ethan Lane <ethan.lan@lvjusd.k12.ca.us>
2018-12-03 13:18:47 -0800

    Water

---
+++ RandomStuff.java
@@ -0,0 +1,15 @@
+
+public class RandomStuff
+{
+    public static void main(){
+        
+    for(int i=0; i<5; i++){
+            
+        double age = (int)(Math.random()*7 + 13);
+        
+        System.out.println("The number is" + age+ "!");
+        
+           
+    }
+    
+}
